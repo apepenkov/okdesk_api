4cc5b88a2340c952696fe0208bc7b8cfa18468b3
apepenkov <apepenkov@users.noreply.github.com>
2024-02-19 12:54:45 +0300

    fix __init__ of api

---
+++ okdesk_api/api/__init__.py
@@ -0,0 +1,27 @@
+from . import (
+    references,
+    price_lists,
+    nomenclature,
+    equipments,
+    shared,
+    issues,
+    maintenance_entities,
+    employees,
+    companies,
+    contacts,
+    agreements,
+)
+
+__all__ = [
+    "references",
+    "price_lists",
+    "nomenclature",
+    "equipments",
+    "shared",
+    "issues",
+    "maintenance_entities",
+    "employees",
+    "companies",
+    "contacts",
+    "agreements",
+]
